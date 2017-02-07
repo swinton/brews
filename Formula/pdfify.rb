84b7ea741ea998e8bb19f736569ca6e39758b1b7
Steve Winton <stevewinton@gmail.com>
2017-02-07 06:01:04 -0800

    Revert "Install gem locally"
    This reverts commit b478d30be835e4488a4a8a158736ae5a3e843b4e.

--- Formula/pdfify.rb
+++ Formula/pdfify.rb
@@ -11,7 +11,7 @@ class Pdfify < Formula
   # Also depends on wkhtmltopdf
 
   def install
-      system "gem", "install", "--local", "mustache"
+      system "gem", "install", "mustache"
       bin.install "bin/pdfify"
       lib.install Dir["lib/*"]
   end
